f95fcc494fe629cc9c96d97d6eea2db849686205
Igor Alexandrovich Ivanov <igoralexandrovich95@mail.ru>
2023-01-31 17:51:20 +0500

    Completed

---
+++ task36/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//
+//[3, 7, 23, 12] -> 19
+//
+//[-4, -6, 89, 6] -> 0
+
+
+//Метод генерации массива случайных чисел
+int[] arrayGeneration (int size, int leftRange, int rightRange) {
+
+    int[] arrayOfRandomNumbers = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < arrayOfRandomNumbers.Length; i++) {
+        arrayOfRandomNumbers[i] = random.Next(leftRange,rightRange);
+    }
+    return arrayOfRandomNumbers;
+}
+
+
+// Метод поиска нечетных чисел
+int FindingOddNumbers(int[] array){
+    int oddNumbersCounter = 0;
+    for (int i = 0; i < array.Length; i++) {
+        if(array[i] % 2 != 0){
+            oddNumbersCounter++;
+        };
+    }
+    System.Console.WriteLine(oddNumbersCounter);
+    return oddNumbersCounter;
+}
+
+
+
+int[] arrayRandomNumbers = arrayGeneration(5, -10, 10);
+FindingOddNumbers(arrayRandomNumbers);
